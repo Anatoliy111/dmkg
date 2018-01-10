de203db14e71db618e492d1219e57b9086e40b21
Anatoliy111 <b.tolik@mail.ru>
2018-01-10 14:36:18 +0200

    commit

--- app/poslug/models/UploadForm.php
+++ app/poslug/models/UploadForm.php
@@ -143,7 +143,7 @@
 				return true;
 
 			} else {
-				Yii::$app->session->setFlash($this->File->name, "Не вдалося відкрити файл:".$uploadPath.$this->File->name." ".$uploadPath.$filename." ".$zip->filename);
+				Yii::$app->session->setFlash($this->File->name, "Не вдалося відкрити файл:".$uploadPath.$this->File->name." ".$uploadPath.$filename."--- ".$zip->filename);
 //                    echo 'ошибка';
 //				Alert::begin(['options' => ['class' => 'alert-danger'],]);
 //				echo "Не вдалося відкрити файл: '$filename'\n";
