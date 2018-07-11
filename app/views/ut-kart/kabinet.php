d17fbca07f8c029e2df8a43654dee4d38fda7b5d
Anatoliy111 <b.tolik@mail.ru>
2018-07-11 15:52:30 +0300

    commit

--- app/views/ut-kart/kabinet.php
+++ app/views/ut-kart/kabinet.php
@@ -17,6 +17,22 @@ use yii\helpers\ArrayHelper;
 
 	/* @var $this yii\web\View */
 /* @var $model app\models\UtKart */
+//<div class="col-xs-12">
+//
+//			<div class="panel panel-success">
+//				<div class="panel-heading">
+//					<h4><?=Yii::$app->formatter->asDate(Yii::$app->session['period'], 'LLLL Y')?><!--</h4>-->
+<!--</div>-->
+<!--<div class="panel-body">-->
+<!--	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.</p>-->
+<!--</div>-->
+<!--<div class="panel-footer">-->
+<!--	Panel Footer-->
+<!--</div>-->
+<!--</div>-->
+<!--</div>-->
+
+
 ?>
 
 	<?php Pjax::begin(); ?>
@@ -175,20 +191,7 @@ use yii\helpers\ArrayHelper;
 			</div>
 
 
-		<div class="col-xs-12">
 
-			<div class="panel panel-success">
-				<div class="panel-heading">
-					<h4><?=Yii::$app->formatter->asDate(Yii::$app->session['period'], 'LLLL Y')?></h4>
-				</div>
-				<div class="panel-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.</p>
-				</div>
-				<div class="panel-footer">
-					Panel Footer
-				</div>
-			</div>
-		</div>
 
 
 		<div class="col-xs-12">
