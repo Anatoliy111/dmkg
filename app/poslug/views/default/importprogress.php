2fa3248a03a40b4543b7255006fd67a9267524f7
Anatoliy111 <bondyuk.a.g@gmail.com>
2018-09-16 15:48:13 +0300

    commit

--- app/poslug/views/default/importprogress.php
+++ app/poslug/views/default/importprogress.php
@@ -74,6 +74,7 @@ if ($DirFiles<>'')
 	for ($i = 0; $i <= count($NameBase)-1; $i++)
 	{
 		$filename = $DirFiles.'/'.$NameBase[$i];
+		die("Error!!! Opening $filename");
 		if (file_exists($filename)) {
 			$dbf = @dbase_open($filename, 0) or die("Error!!! Opening $filename");
 			@dbase_pack($dbf);
