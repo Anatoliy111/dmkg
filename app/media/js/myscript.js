cb7e03688ea79f6008cdf455cc43a27465bfcf74
Anatoliy111 <bondyuk.a.g@gmail.com>
2019-03-14 15:58:34 +0200

    commit

--- app/media/js/myscript.js
+++ app/media/js/myscript.js
@@ -45,6 +45,126 @@ jQuery(document).on('ready', function() {
 
 });
 
+(function ($) {
+    $('#btn-delimp').on('click', function() {
+
+        var keys = $('#gridfile').yiiGridView('getSelectedRows');
+        if (keys.length != 0){
+            var hi= confirm("Ви впевненні що хочете видалити ці файли?");
+            if (hi== true){
+                $.ajax({
+                    url: "/poslug/default/delfile",
+                    type: 'post',
+                    data: {keys},
+                    success: function(s) {
+                        //				alert(s);
+                    }
+
+                });
+
+            }
+        }
+    });
+})(jQuery);
+
+(function ($) {
+    $('#btn-imp').on('click', function() {
+
+        var keys = $('#gridfile').yiiGridView('getSelectedRows');
+
+        $.ajax({
+            url: "/poslug/default/impfile",
+            type: 'post',
+            data: {keys},
+            success: function () {
+                $(function () {
+                    $('#Modalprogress7').show();
+                    $('#Modalprogress7').modal({backdrop: false});
+                    percent = 0;
+                    formclose = 0;
+                    $.ajax({
+                        url: "importprogress",
+                        success: function (data, succ, hhh) {
+                            //$('.results').html(data);
+                            str = data;
+                            if (str.indexOf("Error!!!") < 0) {
+                                refreshProgress(percent = 1, formclose);
+                            }
+                            else
+                                closeImport(str);
+                        }
+                    });
+
+                });
+
+                function refreshProgress(percent, formclose) {
+                    $.ajax({
+                        url: "importdbf",
+                        success: function (data, succ, hhh) {
+                            $('.results').html(percent);
+                            str = data;
+                            if (str.indexOf("Error!!!") >= 0)
+                                formclose = closeImport(str);
+                            if (str.indexOf("End import!!!") >= 0)
+                                formclose = 1;
+                            percent = percent + 1;
+                            $("#upprogress").html('<div class="progress-bar-success progress-bar" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="1000" style="width:' + percent + '%"><span class="sr-only">' + percent + '% Complete</span></div>');
+                            //$("#message").html(data.message);
+                            $("#mess").html("<p>" + percent + "</p>");
+                            // If the process is completed, we should stop the checking process.
+
+                            if ($('#Modalprogress7').is(':visible')) {
+                                if (formclose == 1) {
+                                    $("#mess").html("<p>" + percent + "</p>");
+
+
+                                    $("#Modalprogress7").modal('hide');
+
+
+                                    alert("Импорт завершен");
+                                    window.location.href = url
+
+                                }
+                                else {
+                                    refreshProgress(percent, formclose);
+                                }
+
+                            }
+                            else {
+                                alert("Импорт прерван");
+                                window.clearInterval(timer);
+                            }
+
+
+                        }
+                    });
+                }
+
+                function completed() {
+                    $("#message").html("Completed");
+                    window.clearInterval(timer);
+                }
+
+                function closeImport(str) {
+
+                    $("#Modalprogress7").modal('hide');
+
+                    alert("Импорт прерван ");
+                    window.clearInterval(timer);
+                    return 1;
+
+
+                }
+
+
+            }
+        });
+    });
+})(jQuery);
+
+
+
+
 //function showModalprogress () {
 //    var Modalprogress = $('#Modalprogress');
 //
