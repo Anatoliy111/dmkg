d70d33c1511d02cc844e23c104f2764fbcbdba52
Anatoliy111 <bondyuk.a.g@gmail.com>
2019-03-18 11:35:02 +0200

    commit

--- app/controllers/UtKartController.php
+++ app/controllers/UtKartController.php
@@ -142,7 +142,7 @@ class UtKartController extends Controller
 							'order_id' => $model->id,
 							'version' => '3',
 							'language' => 'uk',
-							'result_url' => yii::$app->request->referrer,
+							'result_url' => 'http://dmkg.com.ua/ut-kart/callback',
 
 							'sandbox' => 1
 						));
@@ -198,6 +198,64 @@ class UtKartController extends Controller
 	return $this->redirect('/ut-kart');
 
 	}
+
+
+	public function actionCallback()
+	{
+
+		$pay = new UtPay();
+		$post = Yii::$app->request->post();
+
+		if( isset($post['data']) && isset($post['signature'])){
+			$public_key = 'i26177975911';
+			$private_key = 'MRRWK7Ao9WlfTPO2TR5tRf8ciXv8OM73dqGHCjZQ';
+
+			$sign = base64_encode( sha1(
+				$private_key .
+				$post['data'] .
+				$private_key
+				, 1 ));
+
+			if ($post['signature']==$sign ){
+				$result= json_decode( base64_decode($post['data']) );
+				// данные вернуться в base64 формат JSON
+				$pay->findOne($result->order_id);
+
+				$pay->status = $result->status;
+				$pay->datestat = new \DateTime("now", new \DateTimeZone('Europe/Kiev'));;
+				$pay->save();
+				$messageLog = [
+					'status' => 'Платеж прошел.',
+					'post' => $post,
+					'payment' =>$pay->id.' '.$pay->status,
+				];
+
+				Yii::info($messageLog, 'payment_success');
+
+			}
+			else{
+				$messageLog = [
+					'status' => 'Платеж не прошел.',
+					'post' => $post
+				];
+
+				Yii::error($messageLog, 'payment_fail');
+
+			}
+		}
+		else{
+			$messageLog = [
+				'status' => 'Платеж не прошел.',
+				'post' => $post
+			];
+
+			Yii::error($messageLog, 'payment_fail');
+		}
+
+		return $this->redirect('/ut-kart');
+
+	}
+
 
 
     public function actionIndex()
