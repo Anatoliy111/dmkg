eff32027ae3b2385dab5a1c2f0d436757eea3661
Anatoliy111 <bondyuk.a.g@gmail.com>
2019-03-25 14:45:21 +0200

    commit

--- app/views/ut-kart/pay.php
+++ app/views/ut-kart/pay.php
@@ -164,12 +164,12 @@ $asset = \app\assets\AppAsset::register($this);
 //                   1 => Html::panel([ 'heading' => 'Panel Title', 'body' => Html::img($asset->baseUrl.'/p24.png', ['alt' => 'Наш логотип']),], Html::TYPE_PRIMARY),
                    1 => Html::beginTag('div', ['class'=>'panel panel-success', 'style'=>'text-align:center']).
                         Html::img($asset->baseUrl.'/p24.png', []).
-                       '<div class="panel-body">Приват 24</div>'.
+                       '<div class="panel-body">Сплатити через Приват 24</div>'.
 //                       '<div class="panel-footer">Комісія 1%, але не менше 3 грн.</div>'.
                         Html::endTag("div"),
                    2 => Html::beginTag('div', ['class'=>'panel panel-success', 'style'=>'text-align:center']).
                         Html::img($asset->baseUrl.'/visa.png', []).
-                       '<div class="panel-body">Інші банки</div>'.
+                       '<div class="panel-body">Інші способи оплати (Карта,LiqPay,Masterpass, інші...)</div>'.
 //                       '<div class="panel-footer">Комісія 2.75%</div>'.
                         Html::endTag("div"),
                ],['inline' => true,'encode'=>FALSE])->label('Виберіть тип оплати') ?>
