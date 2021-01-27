216229af39507be802b944f748c0e36eed1e6c33
Anatoliy111 <bondyuk.a.g@gmail.com>
2021-01-27 17:08:04 +0200

    commit

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -98,33 +98,7 @@ try {
                             ->setSender($botSender)
                             ->setReceiver($receiverId)
                             ->setText('More buttons and styles')
-                            ->setKeyboard(
-                                (new \Viber\Api\Keyboard())
-                                    ->setButtons([
-                                        (new \Viber\Api\Keyboard\Button())
-                                            ->setBgColor('#8074d6')
-                                            ->setTextSize('small')
-                                            ->setTextHAlign('right')
-                                            ->setActionType('reply')
-                                            ->setActionBody('btn-click')
-                                            ->setText('Button 1'),
-
-                                        (new \Viber\Api\Keyboard\Button())
-                                            ->setBgColor('#2fa4e7')
-                                            ->setTextHAlign('center')
-                                            ->setActionType('reply')
-                                            ->setActionBody('btn-click')
-                                            ->setText('Button 2'),
-
-                                        (new \Viber\Api\Keyboard\Button())
-                                            ->setBgColor('#555555')
-                                            ->setTextSize('large')
-                                            ->setTextHAlign('left')
-                                            ->setActionType('reply')
-                                            ->setActionBody('btn-click')
-                                            ->setText('Button 3'),
-                                    ])
-                            )
+                            ->setKeyboard(getMainMenu())
                     );
                     break;
                 //
@@ -224,4 +198,34 @@ try {
         $log->warning('Actual sign: ' . $bot->getSignHeaderValue());
         $log->warning('Actual body: ' . $bot->getInputBody());
     }
+}
+
+function getMainMenu(){
+
+   return (new \Viber\Api\Keyboard())
+        ->setButtons([
+            (new \Viber\Api\Keyboard\Button())
+                ->setBgColor('#8074d6')
+                ->setTextSize('small')
+                ->setTextHAlign('right')
+                ->setActionType('reply')
+                ->setActionBody('btn-click')
+                ->setText('Button 1'),
+
+            (new \Viber\Api\Keyboard\Button())
+                ->setBgColor('#2fa4e7')
+                ->setTextHAlign('center')
+                ->setActionType('reply')
+                ->setActionBody('btn-click')
+                ->setText('Button 2'),
+
+            (new \Viber\Api\Keyboard\Button())
+                ->setBgColor('#555555')
+                ->setTextSize('large')
+                ->setTextHAlign('left')
+                ->setActionType('reply')
+                ->setActionBody('btn-click')
+                ->setText('Button 3'),
+        ]);
+
 }
