153f7fbd352fa1eed0cc22295d5a89dfea22f087
Anatoliy111 <bondyuk.a.g@gmail.com>
2021-01-27 14:36:10 +0200

    commit

--- app/views/viber/webhook.php
+++ app/views/viber/webhook.php
@@ -6,7 +6,7 @@ use Viber\Bot;
 
 /* @var $this yii\web\View */
 $apiKey = '4cca41c0f8a7df2d-744b96600fc80160-bd5e7b2d32cfdc9b'; // <- PLACE-YOU-API-KEY-HERE
-$webhookUrl = 'https://dmkg.com.ua/viber/bot'; // <- PLACE-YOU-HTTPS-URL
+$webhookUrl = 'https://dmkg.com.ua/viber/bot.php'; // <- PLACE-YOU-HTTPS-URL
 try {
     $client = new Client([ 'token' => $apiKey ]);
     $result = $client->setWebhook($webhookUrl);
