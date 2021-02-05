0d98ded92e962ad80a9fb62f55ce306d16522552
Anatoliy111 <bondyuk.a.g@gmail.com>
2021-02-05 10:36:36 +0200

    commit

--- app/views/ut-kart/kabinet.php
+++ app/views/ut-kart/kabinet.php
@@ -257,7 +257,7 @@ yii\bootstrap\Modal::begin([
 								'value'=>Url::to("https://next.privat24.ua/payments/form/%7B%22companyID%22:%222383219%22,%22form%22:%7B%22query%22:%2236188893%22%7D%7D"),
 								'class' => 'btn btn-success btn-lg btn-block',
 							//	'onclick' => "PrePay($abon->id)",
-								'onclick' => "https://next.privat24.ua/payments/form/%7B%22companyID%22:%222383219%22,%22form%22:%7B%22query%22:%2236188893%22%7D%7D",
+							//	'onclick' => "https://next.privat24.ua/payments/form/%7B%22companyID%22:%222383219%22,%22form%22:%7B%22query%22:%2236188893%22%7D%7D",
 							]);
 						}
 						?>
