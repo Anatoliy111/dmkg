732896618deddabc9ef1d4bc223992d5f8f8bd64
Anatoliy111 <bondyuk.a.g@gmail.com>
2021-02-08 16:33:48 +0200

    commit

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -246,6 +246,7 @@ function getMainMenu(){
             (new \Viber\Api\Keyboard\Button())
                 ->setActionType('open-url')
                 ->setActionBody('https://next.privat24.ua/payments/form/%7B%22companyID%22:%222383219%22,%22form%22:%7B%22query%22:%2236188893%22%7D%7D')
+                ->setText('Операції з ос.рахунками')
                 ->setImage("https://dmkg.com.ua/uploads/viber_p24.jpg"),
         ]);
 
