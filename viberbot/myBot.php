ba330b3bc90837fb043df8dff6bccbad5e23f430
Anatoliy111 <bondyuk.a.g@gmail.com>
2021-02-10 10:13:45 +0200

    commit

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -34,22 +34,10 @@ try {
         // first interaction with bot - return "welcome message"
         ->onConversation(function ($event) use ($bot, $botSender, $log) {
             $log->info('onConversation handler');
-            $buttons = [];
-            for ($i = 0; $i <= 8; $i++) {
-                $buttons[] =
-                    (new \Viber\Api\Keyboard\Button())
-                        ->setColumns(1)
-                        ->setActionType('reply')
-                        ->setActionBody('k' . $i)
-                        ->setText('k' . $i);
-            }
             return (new \Viber\Api\Message\Text())
                 ->setSender($botSender)
-                ->setText("Hi, you can see some demo: send 'k1' or 'k2' etc.")
-                ->setKeyboard(
-                    (new \Viber\Api\Keyboard())
-                        ->setButtons($buttons)
-                );
+                ->setText('Вітаємо в вайбер боті! Оберіть потрібну функцію кнопками нижче.')
+                ->setKeyboard(getMainMenu());
         })
         // when user subscribe to PA
         ->onSubscribe(function ($event) use ($bot, $botSender, $log) {
@@ -58,10 +46,11 @@ try {
             $this->getClient()->sendMessage(
                 (new \Viber\Api\Message\Text())
                     ->setSender($botSender)
-                    ->setText('Thanks for subscription!')
+                    ->setText('Дякуємо що підписалися на наш бот! Оберіть потрібну функцію кнопками нижче.')
+                    ->setKeyboard(getMainMenu())
             );
         })
-        ->onText('|btn-click|s', function ($event) use ($bot, $botSender, $log) {
+        ->onText('|info-click|s', function ($event) use ($bot, $botSender, $log) {
             $log->info('click on button');
             $receiverId = $event->getSender()->getId();
             $bot->getClient()->sendMessage(
@@ -69,137 +58,34 @@ try {
                     ->setSender($botSender)
                     ->setReceiver($receiverId)
                     ->setText('you press the button and you ID '.$receiverId)
+                    ->setKeyboard(getMainMenu())
             );
         })
-        ->onText('|k\d+|is', function ($event) use ($bot, $botSender, $log) {
-            $caseNumber = (int)preg_replace('|[^0-9]|s', '', $event->getMessage()->getText());
-            $log->info('onText demo handler #' . $caseNumber);
-            $client = $bot->getClient();
-            $receiverId = $event->getSender()->getId();
-            switch ($caseNumber) {
-                case 0:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Text())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setText('Basic keyboard layout')
-                            ->setKeyboard(
-                                (new \Viber\Api\Keyboard())
-                                    ->setButtons([
-                                        (new \Viber\Api\Keyboard\Button())
-                                            ->setActionType('reply')
-                                            ->setActionBody('btn-click')
-                                            ->setText('Tap this button')
-                                    ])
-                            )
-                    );
-                    break;
-                //
-                case 1:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Text())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setText('More buttons and styles')
-                            ->setKeyboard(getMainMenu())
-                    );
-                    break;
-                //
-                case 2:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Contact())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setName('Novikov Bogdan')
-                            ->setPhoneNumber('+380000000000')
-                            ->setKeyboard(getMainMenu())
-                    );
-                    break;
-                //
-                case 3:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Location())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setLat(48.486504)
-                            ->setLng(35.038910)
-                            ->setKeyboard(getMainMenu())
-                    );
-                    break;
-                //
-                case 4:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Sticker())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setStickerId(114408)
-                            ->setKeyboard(getMainMenu())
-                    );
-                    break;
-                //
-                case 5:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Url())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setMedia('https://hcbogdan.com')
-                            ->setKeyboard(getMainMenu())
-                    );
-                    break;
-                //
-                case 6:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Picture())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setText('some media data')
-                            ->setMedia('https://developers.viber.com/img/devlogo.png')
-                            ->setKeyboard(getMainMenu())
-                    );
-                    break;
-                //
-                case 7:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\Video())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setSize(2 * 1024 * 1024)
-                            ->setMedia('http://techslides.com/demos/sample-videos/small.mp4')
-                            ->setKeyboard(getMainMenu())
-                    );
-                    break;
-                //
-                case 8:
-                    $client->sendMessage(
-                        (new \Viber\Api\Message\CarouselContent())
-                            ->setSender($botSender)
-                            ->setReceiver($receiverId)
-                            ->setButtonsGroupColumns(6)
-                            ->setButtonsGroupRows(6)
-                            ->setBgColor('#FFFFFF')
-                            ->setButtons([
-                                (new \Viber\Api\Keyboard\Button())
-                                    ->setColumns(6)
-                                    ->setRows(3)
-                                    ->setActionType('open-url')
-                                    ->setActionBody('https://www.google.com')
-                                    ->setImage('https://i.vimeocdn.com/portrait/58832_300x300'),
 
-                                (new \Viber\Api\Keyboard\Button())
-                                    ->setColumns(6)
-                                    ->setRows(3)
-                                    ->setActionType('reply')
-                                    ->setActionBody('https://www.google.com')
-                                    ->setText('<span style="color: #ffffff; ">Buy</span>')
-                                    ->setTextSize("large")
-                                    ->setTextVAlign("middle")
-                                    ->setTextHAlign("middle")
-                                    ->setImage('https://s14.postimg.org/4mmt4rw1t/Button.png')
-                            ])
-                    );
-                    break;
-            }
+        ->onText('|rah-menu|s', function ($event) use ($bot, $botSender, $log) {
+            $log->info('click on button');
+            $receiverId = $event->getSender()->getId();
+            $bot->getClient()->sendMessage(
+                (new \Viber\Api\Message\Text())
+                    ->setSender($botSender)
+                    ->setReceiver($receiverId)
+                    ->setText('you press the button and you ID '.$receiverId)
+                    ->setKeyboard(getRahMenu())
+            );
         })
+
+        ->onText('|back|s', function ($event) use ($bot, $botSender, $log) {
+            $log->info('click on button');
+            $receiverId = $event->getSender()->getId();
+            $bot->getClient()->sendMessage(
+                (new \Viber\Api\Message\Text())
+                    ->setSender($botSender)
+                    ->setReceiver($receiverId)
+                 //   ->setText('')
+                    ->setKeyboard(getMainMenu())
+            );
+        })
+
         ->onText('|.*|s', function ($event) use ($bot, $botSender, $log) {
             $log->info('onText ' . var_export($event, true));
             // .* - match any symbols
@@ -208,7 +94,7 @@ try {
                     ->setSender($botSender)
                     ->setReceiver($event->getSender()->getId())
                     ->setText('Вітаємо в вайбер боті! Оберіть потрібну функцію кнопками нижче.')
-                    ->setKeyboard(getMainMenu())
+                    ->setKeyboard(getRahMenu())
             );
         })
         ->run();
@@ -232,9 +118,10 @@ function getMainMenu(){
                 ->setTextHAlign('center')
                 ->setTextVAlign('bottom')
                 ->setActionType('reply')
-                ->setActionBody('btn-click')
-                ->setText('Інформація по ос.рахунках')
-                ->setImage("https://dmkg.com.ua/uploads/plus.png"),
+                ->setActionBody('info-click')
+               ->setBgColor("#f7bb3f")
+                ->setText('Інформація по ос.рахунках'),
+               // ->setImage("https://dmkg.com.ua/uploads/plus.png"),
 
             (new \Viber\Api\Keyboard\Button())
                 ->setColumns(3)
@@ -242,9 +129,9 @@ function getMainMenu(){
                 ->setTextHAlign('center')
                 ->setTextSize('regular')
                 ->setActionType('reply')
-                ->setActionBody('btn-click')
+                ->setActionBody('rah-menu')
                 ->setBgColor("#f7bb3f")
-                ->setImage("https://dmkg.com.ua/uploads/copy.png")
+               // ->setImage("https://dmkg.com.ua/uploads/copy.png")
                 ->setText('<font color=\"#494E67\">Операції з ос.рахунками</font>'),
 
             (new \Viber\Api\Keyboard\Button())
@@ -278,6 +165,15 @@ function getRahMenu(){
                 ->setActionType('reply')
                 ->setActionBody('btn-click')
                 ->setText('Видалити рахунок з бота'),
+
+            (new \Viber\Api\Keyboard\Button())
+                ->setColumns(3)
+                //  ->setBgColor('#2fa4e7')
+                ->setTextHAlign('center')
+                ->setTextSize('large')
+                ->setActionType('reply')
+                ->setActionBody('back')
+                ->setText('< Назад'),
 
         ]);
 
