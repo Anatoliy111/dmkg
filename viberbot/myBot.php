8afc85efcc52e4dffb297ed63562122980b9983a
Anatoliy111 <bondyuk.a.g@gmail.com>
2021-02-11 15:15:34 +0200

    commit

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -156,7 +156,7 @@ function getMainMenu(){
                 ->setColumns(3)
                 ->setActionType('open-url')
                 ->setActionBody('https://next.privat24.ua/payments/form/%7B%22companyID%22:%222383219%22,%22form%22:%7B%22query%22:%2236188893%22%7D%7D')
-                ->setImage("https://dmkg.com.ua/uploads/p24-1.jpg"),
+                ->setImage("https://dmkg.com.ua/uploads/p241.jpg"),
         ]);
 
 }
@@ -192,6 +192,7 @@ function getRahMenu(){
               //  ->setTextSize('large')
                 ->setActionType('reply')
                 ->setActionBody('MainMenu')
+                ->setImage("https://dmkg.com.ua/uploads/home_small.png")
                 ->setText('Головне меню'),
 
         ]);
