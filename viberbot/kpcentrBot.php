9b15beea8d81accbb5e0513176ce53ef6876f783
Anatoliy111 <bondyuk.a.g@gmail.com>
2021-03-15 15:58:44 +0200

    commit

--- viberbot/kpcentrBot.php
+++ viberbot/kpcentrBot.php
@@ -297,7 +297,7 @@ function getMainMenu(){
                 ->setColumns(3)
                 ->setActionType('open-url')
                 ->setActionBody('https://next.privat24.ua/payments/form/%7B%22companyID%22:%222381919%22,%22form%22:%7B%22query%22:%2233006271%22%7D%7D')
-                ->setImage("https://dmkg.com.ua/uploads/privat600x171.png"),
+                ->setImage("https://dmkg.com.ua/uploads/privat800x200.png"),
         ]);
 
 }
