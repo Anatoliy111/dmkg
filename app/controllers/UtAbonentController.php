b0af63050c9229b5b0b0f3a51e8a8605124ef1cf
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-08-09 17:15:31 +0300

    sdfsdf

--- app/controllers/UtAbonentController.php
+++ app/controllers/UtAbonentController.php
@@ -614,13 +614,13 @@ class UtAbonentController extends Controller
             }
 
             if ($modelpokazn->load(Yii::$app->request->post()) && $modelpokazn->validate()) {
-//                $modelpokazn->date_pok = null;
+                $modelpokazn->date_pok = null;
 
-//                $modelpokazn->save();
-                $modelpokazn2 = new Pokazn();
-//                $modelpokazn2->schet = iconv('UTF-8', 'windows-1251', $_SESSION['abon']->schet);
-                $modelpokazn2->pokazn = 760.00;
-                $modelpokazn2->save();
+                $modelpokazn->save();
+//                $modelpokazn2 = new Pokazn();
+////                $modelpokazn2->schet = iconv('UTF-8', 'windows-1251', $_SESSION['abon']->schet);
+//                $modelpokazn2->pokazn = 760.00;
+//                $modelpokazn2->save();
 
 //                Yii::$app->fdb->createCommand("execute procedure calc_pok(:schet)")->bindValue(':schet', $modelabonpokazn->schet)->execute();
 //                $voda = HVoda::find()->where(['schet' => $modelabonpokazn->schet])->orderBy(['kl' => SORT_DESC])->one();
