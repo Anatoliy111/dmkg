bb72867151eb753137b253deebd66dbde6040d25
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-08-09 13:33:51 +0300

    Update UtAbonentController.php

--- app/controllers/UtAbonentController.php
+++ app/controllers/UtAbonentController.php
@@ -616,10 +616,10 @@ class UtAbonentController extends Controller
             if ($modelabonpokazn->load(Yii::$app->request->post()) && $modelabonpokazn->validate()) {
 //                $modelabonpokazn->pokazn = intval($modelabonpokazn->pokazn);
                 $modelabonpokazn1 = new Pokazn();
-                $modelabonpokazn1->schet = iconv('UTF-8', 'windows-1251', $_SESSION['abon']->schet);
+//                $modelabonpokazn1->schet = iconv('UTF-8', 'windows-1251', $_SESSION['abon']->schet);
 //                $modelabonpokazn1->yearmon =$nowdate;
                 $modelabonpokazn1->date_pok = date("Y-m-d");
-                $modelabonpokazn1->vid_pok = 37;
+//                $modelabonpokazn1->vid_pok = 37;
 
 
                 $modelabonpokazn1->save();
