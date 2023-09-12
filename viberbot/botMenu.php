50e8a1ba518daa39cd84d18ac877b4a09ed218a3
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-09-12 08:30:34 +0300

    Update botMenu.php

--- viberbot/botMenu.php
+++ viberbot/botMenu.php
@@ -249,7 +249,7 @@ function infoDmkgSchet($schet){
     $schet1251 = trim(iconv('UTF-8', 'windows-1251', $schet));
 //    $modelKart = DolgKart::findOne(['schet' => trim(iconv('UTF-8', 'windows-1251', $schet))]);
 //    $modelKart = DolgKart::find()->where(['schet' => $schet1251])->all()[0];
-    $period=DolgPeriod::find()->select('period')->orderBy(['period' => SORT_DESC])->one()->period;
+//    $period=DolgPeriod::find()->select('period')->orderBy(['period' => SORT_DESC])->one()->period;
 //    $dolg=Yii::$app->dolgdb->createCommand('select vw_obkr.*,round((dolg-fullopl),2) as dolgopl from vw_obkr where period=\''.$period.'\' and schet=\''.$schet1251.'\' order by npp')->QueryAll();
 
     $mess = 'Особовий рахунок - '.$schet1251."\r\n";
