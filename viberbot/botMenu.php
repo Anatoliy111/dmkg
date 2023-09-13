55ceaed455048c5b5d7b20784591419a241b3a96
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-09-13 15:40:12 +0300

    Update botMenu.php

--- viberbot/botMenu.php
+++ viberbot/botMenu.php
@@ -249,10 +249,10 @@ function infoDmkgSchet($schet){
     try {
 
     $schet1251 = trim(iconv('UTF-8', 'windows-1251', $schet));
-        if ($schet=='0030009м') return $schet1251;
-//    $modelKart = DolgKart::findOne(['schet' => trim(iconv('UTF-8', 'windows-1251', $schet))]);
-//    $modelKart = DolgKart::find()->where(['schet' => $schet1251])->all()[0];
-//    $period=DolgPeriod::find()->select('period')->orderBy(['period' => SORT_DESC])->one()->period;
+    if ($schet=='0030009м') return $schet1251;
+//  $modelKart = DolgKart::findOne(['schet' => trim(iconv('UTF-8', 'windows-1251', $schet))]);
+//  $modelKart = DolgKart::find()->where(['schet' => $schet1251])->all()[0];
+//  $period=DolgPeriod::find()->select('period')->orderBy(['period' => SORT_DESC])->one()->period;
     $period=Yii::$app->dolgdb->createCommand('select first 1 period from period order by period desc')->QueryAll();
     $dolg=Yii::$app->dolgdb->createCommand('select vw_obkr.*,round((dolg-fullopl),2) as dolgopl from vw_obkr where period=\''.$period[0]["period"].'\' and schet=\''.$schet1251.'\' order by npp')->QueryAll();
 //
