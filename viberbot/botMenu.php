1ef1ba5413b2380722f3c488766bc769c939ae2b
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-09-13 15:33:35 +0300

    Update botMenu.php

--- viberbot/botMenu.php
+++ viberbot/botMenu.php
@@ -247,8 +247,9 @@ function infoDmkgSchet($schet){
 
     $mess='';
     try {
-    if ($schet=='0030009м') return $schet;
+
     $schet1251 = trim(iconv('UTF-8', 'windows-1251', $schet));
+        if ($schet=='0030009м') return $schet1251;
 //    $modelKart = DolgKart::findOne(['schet' => trim(iconv('UTF-8', 'windows-1251', $schet))]);
 //    $modelKart = DolgKart::find()->where(['schet' => $schet1251])->all()[0];
 //    $period=DolgPeriod::find()->select('period')->orderBy(['period' => SORT_DESC])->one()->period;
