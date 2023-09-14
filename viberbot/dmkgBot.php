6c909af3c6bb022ea7115db452d6ed84c2de2e50
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-09-14 09:01:23 +0300

    Update dmkgBot.php

--- viberbot/dmkgBot.php
+++ viberbot/dmkgBot.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 //require_once(__DIR__ . '/../yii');
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-$yiiConfig = require __DIR__ . '/../app/config/console.php';
+$yiiConfig = require __DIR__ . '/../app/config/web.php';
 new yii\web\Application($yiiConfig);
 
 
