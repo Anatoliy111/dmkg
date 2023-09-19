686a12b13494c34c4122e5b950087ccb4b2a2b2a
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-09-19 09:40:24 +0300

    Update myBot.php

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -676,9 +676,9 @@ function infoSchetOS($schet) {
         $fio = trim(iconv('windows-1251', 'UTF-8',$dolg[0]["fio"]));
         $mess = $mess.$fio . "\n";
 
-        $mess = $mess.trim(iconv('windows-1251', 'UTF-8', $dolg[0]["ulnaim"])).' буд.'.trim(iconv('windows-1251', 'UTF-8', $dolg[0]["nomdom"])).' '.(isset($dolg[0]["nomkv"])?'кв.'.$dolg[0]["nomkv"]:'')."\r\n";
+//        $mess = $mess.trim(iconv('windows-1251', 'UTF-8', $dolg[0]["ulnaim"])).' буд.'.trim(iconv('windows-1251', 'UTF-8', $dolg[0]["nomdom"])).' '.(isset($dolg[0]["nomkv"])?'кв.'.$dolg[0]["nomkv"]:'')."\r\n";
         $mess = $mess.'----------------------------'."\n";
-        return $mess;
+
         $mess = $mess.Yii::$app->formatter->asDate($period[0]["period"], 'LLLL Y')."\n\r";
         $mess = $mess.'----------------------------'."\n";
         $mess = $mess.'Ваша заборгованість по послугам:'."\n\r";
