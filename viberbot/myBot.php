306fbd59833506602427d03a38750f6e5eefe632
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-09-19 10:55:13 +0300

    Update myBot.php

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -346,7 +346,9 @@ function getDmkgMenuOS(){
                 ->setActionType('open-url')
                 ->setActionBody('https://next.privat24.ua/payments/form/%7B%22companyID%22:%222383219%22,%22form%22:%7B%22query%22:%2236188893%22%7D%7D')
                 ->setImage("https://dmkg.com.ua/uploads/privat800x200.png")
+                ->setTextSize('small')
                 ->setTextHAlign('right')
+                ->setTextVAlign('right')
                 ->setText('Оплата'),
         ]);
 
