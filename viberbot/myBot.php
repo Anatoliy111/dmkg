ac03e8f9b9759b74efe4696a87443ed8aa172fde
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-11-15 14:15:23 +0200

    Update myBot.php

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -71,7 +71,7 @@ try {
             return (new \Viber\Api\Message\Text())
                 ->setSender($botSender)
                 ->setText($mes)
-                ->setKeyboard(getDmkgMenuOS(null));
+                ->setKeyboard(getDmkgMenuOS());
 
             // $mes = 'Вітаємо в вайбер боті! Оберіть потрібну функцію кнопками нижче.';
 //            message($bot, $botSender, $event, 'Вітаємо в вайбер боті! Оберіть потрібну функцію кнопками нижче.', getDmkgMenuOS($Receiv));
