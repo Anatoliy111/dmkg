d9b54b6da15bf5a097a5781d0e54b520c62e5dca
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-11-16 09:33:02 +0200

    Update myBot.php

--- viberbot/myBot.php
+++ viberbot/myBot.php
@@ -63,19 +63,35 @@ try {
 //            $Receiv = verifyReceiver($event, $apiKey, $org);
 //            if ($Receiv<>null) $FindModels = ViberAbon::find()->where(['id_viber' => $Receiv->id]);
 
-            $mes = ' Вітаємо вас в вайбер боті КП "ДМКГ"!!!'."\n";
-            $mes = $mes.' Натисніть кнопку Почати"!!!'."\n";
+//            $mes = ' Вітаємо вас в вайбер боті КП "ДМКГ"!!!'."\n";
+
 
 //            if (($FindModels == null) and ($Receiv->id_abonent == null)) {
 //                $mes=$mes.'Пройдіть процедуру реєстрації, обравши кнопку Авторизація/Реєстрація';
 //            }
 //            if ($Receiv==null) $mes='null';
 //            else $mes='not null';
+            try{
+                $Receiv = verifyReceiver($event, $apiKey, $org);
+            } finally {
+                $mes = ' Вітаємо вас в вайбер боті КП "ДМКГ"!!!'."\n";
+            }
+            if ($Receiv<>null){
+                $mes = $mes.'Оберіть потрібну функцію кнопками нижче.';
+                return (new \Viber\Api\Message\Text())
+                    ->setSender($botSender)
+                    ->setText($mes)
+                    ->setKeyboard(getDmkgMenuOS($Receiv));
+            }else{
+                $mes = $mes.' Натисніть кнопку Почати"!!!'."\n";
 
-            return (new \Viber\Api\Message\Text())
-                ->setSender($botSender)
-                ->setText($mes)
-                ->setKeyboard(getDmkgMenuStart());
+                return (new \Viber\Api\Message\Text())
+                    ->setSender($botSender)
+                    ->setText($mes)
+                    ->setKeyboard(getDmkgMenuStart());
+            }
+
+
 
             // $mes = 'Вітаємо в вайбер боті! Оберіть потрібну функцію кнопками нижче.';
 //            message($bot, $botSender, $event, 'Вітаємо в вайбер боті! Оберіть потрібну функцію кнопками нижче.', getDmkgMenuOS($Receiv));
