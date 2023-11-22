41bf2f174c4f908f0a652df4fe457edaa47252ef
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-11-22 14:23:19 +0200

    Update SiteController.php

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -27,7 +27,7 @@ use yii\web\Controller;
 use yii\web\ForbiddenHttpException;
 
 //require (Yii::getAlias('@webroot'). '/viberbot/mySendBot.php');
-require (Yii::getAlias('@webroot'). '/viberbot/botMenu.php');
+//require (Yii::getAlias('@webroot'). '/viberbot/botMenu.php');
 //require (Yii::getAlias('@webroot'). '/viberbot/kpcentrBot.php');
 //require (Yii::getAlias('@webroot'). '/viberbot/kpcentrBot.php');
 
