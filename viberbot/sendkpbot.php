a00118f3990283bedf8956560164f9615587a379
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-11-22 11:10:52 +0200

    Update sendkpbot.php

--- viberbot/sendkpbot.php
+++ viberbot/sendkpbot.php
@@ -51,9 +51,9 @@ $receivid = 'WhD/fEV4c9rtUBGOAihTqA==';
 $FindModels = Viber::findAll(['api_key' => $apiKey]);
 
 foreach ($FindModels as $model) {
-    if ($model->id_receiver == $receivid) {
+//    if ($model->id_receiver == $receivid) {
         send($apiKey,$botSender,$log,$message,$model->id_receiver);
-    }
+//    }
 }
 
 
@@ -96,6 +96,8 @@ function send($apiKey,$botSender,$log,$message,$receivid)
             $log->warning('Actual body: ' . $bot->getInputBody());
         }
     }
+
+    return '';
 }
 
 
