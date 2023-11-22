8c6cf2def2a51655e61c1a424cf419654894715e
Anatoliy111 <bondyuk.a.g@gmail.com>
2023-11-22 09:03:38 +0200

    іваівп

---
+++ viberbot/sendkpbot.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Пользователь
+ * Date: 17.02.2021
+ * Time: 15:44
+ */
+
+
+use Viber\Bot;
+use Viber\Api\Sender;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+$yiiConfig = require __DIR__ . '/../app/config/web.php';
+new yii\web\Application($yiiConfig);
+require_once(__DIR__ . '\dmkgMenuSend.php');
+
+
+//$apiKey = '4d2db29edaa7d108-28c0c073fd1dca37-bc9a431e51433742'; //dmkgBot
+$apiKey = '4cca41c0f8a7df2d-744b96600fc80160-bd5e7b2d32cfdc9b';  //myBot
+
+$botSender = new Sender([
+    'name' => 'bondyukViberBot',
+    'avatar' => '',
+]);
+
+// log bot interaction
+$log = new Logger('bot');
+$log->pushHandler(new StreamHandler(__DIR__ .'/tmp/bot.log'));
+
+//$message='Доброго дня! Повідомляємо що з  01.12.2023р. вайбербот KPCentrBot припинить працювати. Для подачі показників по воді реєструйтесь у вайберботі ДМКГ DmkgBot натиснувши на посилання viber://pa?chatURI=dmkgBot або реєструйтесь в кабінеті споживача на сайті dmkg.com.ua. При виникненні проблем з реєстрацією звертайтесь в кабінет ЕКОНОМІСТИ в приміщенні Долинського Міськомунгоспу за адресою м.Долинська вул.Нова 80-а. До кінця листопада 2023, бот буде ПРАЦЮВАТИ та приймати показники!!!';
+
+$message = <<<EOD
+Доброго дня! Повідомляємо що з  01.12.2023р. вайбербот KPCentrBot припинить працювати.
+Для подачі показників по воді реєструйтесь у вайберботі ДМКГ DmkgBot натиснувши на посилання
+viber://pa?chatURI=dmkgBot або реєструйтесь в кабінеті споживача на сайті dmkg.com.ua (вхід за ел.поштою).
+При виникненні проблем з реєстрацією звертайтесь в кабінет ЕКОНОМІСТИ в приміщенні Долинського Міськомунгоспу за адресою м.Долинська вул.Нова 80-а.
+До кінця листопада 2023, бот буде ПРАЦЮВАТИ та приймати показники!!!
+EOD;
+
+try {
+    // create bot instance
+    $bot = new Bot(['token' => $apiKey]);
+    $bot->getClient()->sendMessage(
+        (new \Viber\Api\Message\Text())
+            ->setSender($botSender)
+            ->setReceiver('gN0uFHnqvanHwb17QuwMaQ==')
+            ->setText($message)
+    );
+
+} catch (Exception $e) {
+    $log->warning('Exception: ' . $e->getMessage());
+    if ($bot) {
+        $log->warning('Actual sign: ' . $bot->getSignHeaderValue());
+        $log->warning('Actual body: ' . $bot->getInputBody());
+    }
+}
+
+
+
+
+
+
