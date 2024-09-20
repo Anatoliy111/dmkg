da1ff8e6e0f626d847b61f0fcb393de59c0f1e74
Anatoliy111 <bondyuk.a.g@gmail.com>
2024-09-20 15:36:22 +0300

    Update dmkgBot.php

--- viberbot/dmkgBot.php
+++ viberbot/dmkgBot.php
@@ -380,8 +380,6 @@ try {
                     try {
                         $ModelKart = DolgKart::findOne(['schet' => trim(iconv('UTF-8', 'windows-1251', $match[0][1]))]);
                         if ($ModelKart != null) {
-                            getSend(mb_strtolower(trim(iconv('windows-1251', 'UTF-8', $ModelKart->fio))));
-                            getSend(mb_strtolower(trim($event->getMessage()->getText())));
                             if (mb_strtolower(trim(iconv('windows-1251', 'UTF-8', $ModelKart->fio))) == mb_strtolower(trim($event->getMessage()->getText()))) {
                                 $addabon = addAbonkart($Receiv, $match[0][1]);
                                 if ($addabon != null) message($bot, $botSender, $event, 'Вітаємо!!! Рахунок ' . $match[0][1] . ' під"єднано до кабінета.', getRahMenu());
