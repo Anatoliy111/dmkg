cb01cfe4df3856eb9bbb2ca0c8c36b72603c1e7e
Anatoliy111 <bondyuk.a.g@gmail.com>
2025-05-22 16:05:35 +0300

    commit

--- app/models/Pokazn.php
+++ app/models/Pokazn.php
@@ -56,45 +56,45 @@ class Pokazn extends \yii\db\ActiveRecord
             [['pokazn', 'ppp'], 'number'],
             [['date_pok', 'date_zn'], 'safe'],
             [['schet'], 'string', 'max' => 10],
-            [['fio'], 'string', 'max' => 64],
-            [['pokazn'], function ($attribute) {
-                $session = Yii::$app->session;
-                $pok = Pokazn::find()->where(['schet' => $this->schet])->andWhere(['or', ['del' => 0], ['del' => null]])->orderBy(['id' => SORT_DESC])->one();
-                $this->lastpokazn = $pok->pokazn;
-                $kub = $this->pokazn-$this->lastpokazn;
-                $period = $_SESSION['period'];
-                if ($this->pokazn<$this->lastpokazn) {
-                    $this->addError($attribute, "Ваш показник меньший за останній зареєстрований показник ".$this->lastpokazn."! Спробуйте ще!!!");
-                }
-                else {
-                    if ($kub > 100 and $this->vid_pok <> 21) {
-                        if (isset($session['bigkub'])) {
-                            //                   if (array_key_exists('bigkub',$session)) {
-                            if ($kub <> $session['bigkub']) {
-                                $this->addError($attribute, "Ваш показник " . $this->pokazn . " перевищує 100 кубів!!! Бажаєте продовжити(зберегти) ?");
-                                $session->set('bigkub', $kub);
-                                //                          $session['bigkub'] = $kub;
-                            }
-                        } else {
-                            $this->addError($attribute, "Ваш показник " . $this->pokazn . " перевищує 100 кубів !!! Бажаєте продовжити(зберегти)?");
-                            $session->set('bigkub', $kub);
-                        }
-                    } else {
-                        $session->set('bigkub', 0);
-//                       // $poksite = UtAbonpokazn::find()->where(['schet' => iconv('windows-1251', 'UTF-8', $this->schet)])->orderBy(['date_ins' => SORT_DESC])->one();
-                        $poksite = UtAbonpokazn::find()->where(['schet' => iconv('windows-1251', 'UTF-8', $this->schet)])->andwhere(['>=','data',$period])->orderBy(['date_ins' => SORT_DESC])->one();
-                        if ($poksite <> null)
-                            if ($poksite->data == $this->date_pok) {
-                                if ($poksite->vid == 'site') $this->addError($attribute, "Ви вже сьогодні подали показник " . $poksite->pokazn . " через кабінет споживача!!! За один день здаємо тільки один показник!");
-                                if ($poksite->vid == 'viber') $this->addError($attribute, "Ви вже сьогодні подали показник " . $poksite->pokazn . " через ViberBot!!! За один день здаємо тільки один показник!");
-                            } elseif ($this->pokazn <= $poksite->pokazn) {
-                                if ($poksite->vid == 'site') $this->addError($attribute, "Ви вже подали показник " . $poksite->pokazn . ' ' . $poksite->data . " через кабінет споживача!!!");
-                                if ($poksite->vid == 'viber') $this->addError($attribute, "Ви вже подали показник " . $poksite->pokazn . ' ' . $poksite->data . " через ViberBot!!!");
-                            }
-                    }
-                }
-
-            }],
+       //     [['fio'], 'string', 'max' => 64],
+//            [['pokazn'], function ($attribute) {
+//                $session = Yii::$app->session;
+//                $pok = Pokazn::find()->where(['schet' => $this->schet])->andWhere(['or', ['del' => 0], ['del' => null]])->orderBy(['id' => SORT_DESC])->one();
+//                $this->lastpokazn = $pok->pokazn;
+//                $kub = $this->pokazn-$this->lastpokazn;
+//                $period = $_SESSION['period'];
+//                if ($this->pokazn<$this->lastpokazn) {
+//                    $this->addError($attribute, "Ваш показник меньший за останній зареєстрований показник ".$this->lastpokazn."! Спробуйте ще!!!");
+//                }
+//                else {
+//                    if ($kub > 100 and $this->vid_pok <> 21) {
+//                        if (isset($session['bigkub'])) {
+//                            //                   if (array_key_exists('bigkub',$session)) {
+//                            if ($kub <> $session['bigkub']) {
+//                                $this->addError($attribute, "Ваш показник " . $this->pokazn . " перевищує 100 кубів!!! Бажаєте продовжити(зберегти) ?");
+//                                $session->set('bigkub', $kub);
+//                                //                          $session['bigkub'] = $kub;
+//                            }
+//                        } else {
+//                            $this->addError($attribute, "Ваш показник " . $this->pokazn . " перевищує 100 кубів !!! Бажаєте продовжити(зберегти)?");
+//                            $session->set('bigkub', $kub);
+//                        }
+//                    } else {
+//                        $session->set('bigkub', 0);
+////                       // $poksite = UtAbonpokazn::find()->where(['schet' => iconv('windows-1251', 'UTF-8', $this->schet)])->orderBy(['date_ins' => SORT_DESC])->one();
+//                        $poksite = UtAbonpokazn::find()->where(['schet' => iconv('windows-1251', 'UTF-8', $this->schet)])->andwhere(['>=','data',$period])->orderBy(['date_ins' => SORT_DESC])->one();
+//                        if ($poksite <> null)
+//                            if ($poksite->data == $this->date_pok) {
+//                                if ($poksite->vid == 'site') $this->addError($attribute, "Ви вже сьогодні подали показник " . $poksite->pokazn . " через кабінет споживача!!! За один день здаємо тільки один показник!");
+//                                if ($poksite->vid == 'viber') $this->addError($attribute, "Ви вже сьогодні подали показник " . $poksite->pokazn . " через ViberBot!!! За один день здаємо тільки один показник!");
+//                            } elseif ($this->pokazn <= $poksite->pokazn) {
+//                                if ($poksite->vid == 'site') $this->addError($attribute, "Ви вже подали показник " . $poksite->pokazn . ' ' . $poksite->data . " через кабінет споживача!!!");
+//                                if ($poksite->vid == 'viber') $this->addError($attribute, "Ви вже подали показник " . $poksite->pokazn . ' ' . $poksite->data . " через ViberBot!!!");
+//                            }
+//                    }
+//                }
+//
+//            }],
         ];
     }
 
@@ -115,8 +115,8 @@ class Pokazn extends \yii\db\ActiveRecord
             'vid_zn' => 'Vid Zn',
             'schet' => 'Schet',
             'id_lich' => 'Id Lich',
-            'ppp' => 'Ppp',
-            'fio' => 'ПІП'
+            'ppp' => 'Ppp'
+       //     'fio' => 'ПІП'
         ];
     }
 
