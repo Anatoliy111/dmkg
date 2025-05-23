e3bce5ec3227824b26095393318a8a89d177fc83
Anatoliy111 <bondyuk.a.g@gmail.com>
2025-05-23 15:50:12 +0300

    commit

--- app/controllers/UtAbonentController.php
+++ app/controllers/UtAbonentController.php
@@ -707,7 +707,7 @@ class UtAbonentController extends Controller
         $modelpokazn->schet = $_SESSION['abon']->schet;
         $modelpokazn->yearmon =$nowdate;
         $modelpokazn->fio = trim(iconv('UTF-8','windows-1251',$_SESSION['model']->fio));
-     //   $modelpokazn->date_pok = date("Y-m-d");
+        $modelpokazn->date_pok = date('d.m.Y');
         $modelpokazn->vid_pok = 37;
         $modelpokazn->pokazn = 1024;
         $modelpokazn->pokaznik = 1024;
